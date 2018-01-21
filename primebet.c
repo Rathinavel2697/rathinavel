992d9266b60057ab871ce17b619d0d612f257e59
Rathinavel2697 <35128000+Rathinavel2697@users.noreply.github.com>
2018-01-21 11:55:05 +0530

    Update primebet.c

--- primebet.c
+++ primebet.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 int main()
 {
-    int low, high, i, flag;
+    int low, high, i, f;
     printf("Enter two numbers(intervals): ");
     scanf("%d %d", &low, &high);
 
@@ -9,18 +9,18 @@ int main()
 
     while (low < high)
     {
-        flag = 0;
+        f = 0;
 
         for(i = 2; i <= low/2; ++i)
         {
             if(low % i == 0)
             {
-                flag = 1;
+                f = 1;
                 break;
             }
         }
 
-        if (flag == 0)
+        if (f == 0)
             printf("%d ", low);
 
         ++low;
