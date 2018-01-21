55296ef1cac6d4f238eba93f7b7e3b1d01679a68
Rathinavel2697 <35128000+Rathinavel2697@users.noreply.github.com>
2018-01-21 11:11:13 +0530

    Update powerofnumbers.c

--- powerofnumbers.c
+++ powerofnumbers.c
@@ -1,20 +1,20 @@
 #include <stdio.h>
 int main()
 {
-    int base, exponent;
+    int base, power;
 
     long long result = 1;
 
     printf("Enter a base number: ");
     scanf("%d", &base);
 
-    printf("Enter an exponent: ");
-    scanf("%d", &exponent);
+    printf("Enter an power: ");
+    scanf("%d", &power);
 
-    while (exponent != 0)
+    while (power != 0)
     {
         result *= base;
-        --exponent;
+        --power;
     }
 
     printf("Answer = %lld", result);
