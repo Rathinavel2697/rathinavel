69c0152d00111859f6b4f49365eca67df54e29fc
Rathinavel2697 <35128000+Rathinavel2697@users.noreply.github.com>
2018-01-21 11:01:22 +0530

    Create sumofteonumberinarray.c

---
+++ sumofteonumberinarray.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+int main()
+{
+    int A[6] = {1, 4, 45, 6, 10, -8};
+    int n;
+    int arr_size = 6;
+    
+    if( hasArrayTwoCandidates(A, arr_size, n))
+ 
+        printf("Array has two elements with sum ");
+    
+    else
+        
+        	printf("Array doesn't have two elements with sum ");
+      
+    return 0;
+
+}
