763a7d947b9de8a1beb729bf32c9489b360054ab
Rathinavel2697 <35128000+Rathinavel2697@users.noreply.github.com>
2018-01-21 12:06:15 +0530

    Update armstrongbet.c

--- armstrongbet.c
+++ armstrongbet.c
@@ -3,7 +3,7 @@
 
 int main()
 {
-    int low, high, i, temp1, temp2, remainder, n = 0, result = 0;
+    int low, high, i, temp1, temp2, remainder, l = 0, result = 0;
 
     printf("Enter two numbers(intervals): ");
     scanf("%d %d", &low, &high);
@@ -14,28 +14,28 @@ int main()
         temp2 = i;
         temp1 = i;
 
-        // number of digits calculation
+       
         while (temp1 != 0)
         {
             temp1 /= 10;
-            ++n;
+            ++l;
         }
 
-        // result contains sum of nth power of its digits
+       
         while (temp2 != 0)
         {
             remainder = temp2 % 10;
-            result += pow(remainder, n);
+            result += pow(remainder, l);
             temp2 /= 10;
         }
 
-        // checks if number i is equal to the sum of nth power of its digits
+       
         if (result == i) {
             printf("%d ", i);
         }
 
-        // resetting the values to check Armstrong number for next iteration
-        n = 0;
+       
+        l = 0;
         result = 0;
 
     }
