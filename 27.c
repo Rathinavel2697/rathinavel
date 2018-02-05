d966f50dde59746e1e4a58c0709bcb1f6beb577f
Rathinavel2697 <35128000+Rathinavel2697@users.noreply.github.com>
2018-02-05 16:59:50 +0530

    Create 27.c

---
+++ 27.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+    int main()
+    {
+    	int a[50],i,j,n;
+     
+    	scanf("%d",&n);
+    	for(i=0;i<n;i++)
+    	{
+    		scanf("%d",&a[i]);
+    	}
+    	for(i=0;i<n;i++)
+    	{
+    		for(j=i;j<n;j++)
+    		{
+    			printf("%d %d",a[i],i);
+    		}
+    	}
+    	return 0;
+    }
