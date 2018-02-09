30dd7ece581d394a23bbf14b73eb9ceb62b9da23
Rathinavel2697 <35128000+Rathinavel2697@users.noreply.github.com>
2018-02-09 16:05:31 +0530

    Create 33.c

---
+++ 33.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main()
+{
+	int i,count=0;
+	char ch[50];
+	scanf("%[^/n]s",ch);
+	for(i=0;ch[i]!='\0';i++)
+	{
+		if(ch[i]==' ' )
+		{
+			count++;
+		}
+	}
+	printf("%d",count);
+	return 0;
+}
+	
