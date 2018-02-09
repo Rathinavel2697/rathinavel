a01c69c54a029262e86b012a6f4e1a6a3ed24fb9
Rathinavel2697 <35128000+Rathinavel2697@users.noreply.github.com>
2018-02-09 17:00:36 +0530

    Create 361.c

---
+++ 361.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+#include<string.h>
+int main()
+{
+	char s[50];
+	int i,n,count=0;
+	gets(s);
+	n=strlen(s);
+	for(i=0;i<n;i++)
+	{
+	if((s[i]>='0'&&s[i]<='9')||(s[i]>='a'&&s[i]<='z')||(s[i]>='A'&&s[i]<='Z'));
+		
+	else
+	count++;
+		
+	}
+
+		
+	printf("%d",count);
+	
+	return 0;
+}
