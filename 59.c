487f8897180648cbdb7dcc88e10d9f962d43d5c1
Rathinavel2697 <35128000+Rathinavel2697@users.noreply.github.com>
2018-03-02 16:44:30 +0530

    Create 59.c

---
+++ 59.c
@@ -0,0 +1,23 @@
+
+int main(void) {
+
+int a[50],i,j,t=0;
+for(i=0;i<10;i++)
+{
+	scanf("%d",&a[i]);
+}
+for(i=0;i<10;i++)
+{
+	for(j=i+1;j<10;j++)
+	{
+		if(a[i]>a[j])
+		{
+			t=a[i];
+			a[i]=a[j];
+			a[j]=t;
+		}
+	}
+}
+printf("%d\n",a[9]);
+	return 0;
+}
