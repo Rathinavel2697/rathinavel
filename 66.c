b60acbc93e08a6c36f5c2e17e5ddbf839ed261c7
Rathinavel2697 <35128000+Rathinavel2697@users.noreply.github.com>
2018-03-16 15:40:10 +0530

    Update 66.c

--- 66.c
+++ 66.c
@@ -1,16 +1,16 @@
 #include<stdio.h>
 int main()
 {
-	int n,i,count;
+	int n,i,count=0;
 	scanf("%d",&n);
-	for(i=0;i<n;i++)
+	for(i=2;i<n/2;i++)
 	{
 		if(n%i==0)
 		{
 			count++;
 		}
 	}
-	if(count==2)
+	if(count==0)
 	{
 		printf("yes\n");
 	}
